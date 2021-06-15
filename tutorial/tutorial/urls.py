1c0755ee528a0d790ddc29189092f64fe6896357
Haris Ahmad <hariis18896@gmail.com>
2021-06-15 09:56:11 -0700

    The empty path didn’t match any of these.

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,12 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -28,6 +22,5 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
-    path('routes/', include(router.urls)),
-    path('api_auth/', include('rest_framework.urls' , namespace='rest_framework')),
+
 ]
