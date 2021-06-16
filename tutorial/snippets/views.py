03d9ff289e778bdddd387906c5189abf22ba53b9
Haris Ahmad <hariis18896@gmail.com>
2021-06-16 04:49:41 -0700

    Now working with Generic Class Based Views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,46 +1,50 @@
 from rest_framework import status
-from rest_framework.decorators import api_view  # The @api_view decorator for working with function based views., and The APIView class for working with class-based views.
+from rest_framework.decorators import APIView   # The APIView class for working with class-based views.
 from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
                                                 # request.POST  # Only handles form data.  Only works for 'POST' method.                                                
-@api_view(['GET','POST'])
-def snippet_list(request, format=None):         # To take advantage of the fact that our responses are no longer hardwired to a single content type let's add support for format suffixes to our API endpoints. Using format suffixes gives us URLs that explicitly refer to a given format, and means our API will be able to handle URLs such as http://example.com/api/items/4.json. and then add suffixes to snippets/urls.py
-    # list all code snippets or create a new snippet
-    if request.method == 'GET':
+
+
+class SnippetList(APIView):
+    # List all snippets, or create a new snippet.
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)    # # Renders to content type as requested by the client.
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = SnippetSerializer(data=request.data)   # request.data  # Handles arbitrary data.  Works for 'POST', 'PUT' and 'PATCH' methods.
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # Using numeric HTTP status codes in your views doesn't always make for obvious reading, and it's easy to not notice if you get an error code wrong. REST framework provides more explicit identifiers for each status code, such as HTTP_400_BAD_REQUEST
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # We'll also need a view which corresponds to an individual snippet, and can be used to retrieve, update or delete the snippet.
-@api_view(['GET','PUT','DELETE'])
-def snippet_detail(request, pk, format=None):
-    # retrive, update or delete a code snippet
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class SnippetDetail(APIView):
+    # Retrieve, update or delete a snippet instance.
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)  # Notice that we're no longer explicitly tying our requests or responses to a given content type. request.data can handle incoming json requests, but it can also handle other formats. Similarly we're returning response objects with data, but allowing REST framework to render the response into the correct content type for us.
-    
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
