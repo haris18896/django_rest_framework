26eaa2c6d8aee080c25cbb48a65c2a759a6b4811
asfanda <asfandyarsaeed@hotmail.com>
2022-01-08 23:50:21 +0500

    Update models.py

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from pygments.lexers import get_all_lexers, get_lexer_by_name  # # We'll be using this for the code highlighting   
 from pygments.styles import get_all_styles
 from pygments.formatters.html import HtmlFormatter
-from pygments import highlight
+
+ory ga rora cha taba dy kom
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
